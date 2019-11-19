cb6cc227f6dfed517c6776a221bc2231a3a6fb34
myrrlyn <self@myrrlyn.dev>
2019-11-19 16:28:58 -0700

    Add `TryConv` as a complement to `Conv`

--- src/conv.rs
+++ src/conv.rs
@@ -8,6 +8,8 @@ parameter in the function declaration, making the trait non-generic and allowing
 the function to be called in method-call position without ambiguity.
 !*/
 
+use core::convert::TryInto;
+
 /** Directed Type Conversion
 
 This trait is an accessory to [`From`] and [`Into`]. It works by moving the
@@ -102,3 +104,103 @@ pub trait Conv: Sized {
 }
 
 impl<T: Sized> Conv for T {}
+
+/** Directed Fallible Type Conversion
+
+This trait is an accessory to [`TryFrom`] and [`TryInto`]. It works by moving
+the destination type from the trait name (`TryInto<Target>::try_into`) into the
+method name (`TryConv::try_conv::<Target>`). This change makes `TryInto<_>` the
+correct trait to use in trait bounds and `.try_conv::<_>` the correct method to
+use in expressions.
+
+A `try_conv::<T>` method is automatically available whenever a `TryInto<T>`
+implementation exists for a type. `TryInto<T>` is most commonly implemented by
+taking advantage of the reflexive blanket implentation using `TryFrom`, but can
+also be manually implemented as desired.
+
+`.try_into()` cannot be used in intermediate expressions, because it is
+impossible for the compiler’s type engine to select a unique `TryInto<T>`
+implementation. This means that expressions like `v.try_into().use()` will never
+compile. Users can replace `.try_into()` with `.try_conv::<Dest>()` in order to
+inform the compiler of the type of the expression after the conversion, and make
+compilation succeed.
+
+`TryConv` cannot be used in trait bounds, because the trait itself is not
+generic. All `Sized` types implement `TryConv` by default, so specifying that a
+type must be `TryConv` adds no information to the solver.
+
+# Examples
+
+## Conversion as methods
+
+Conversion with `.try_into()` will fail to compile, even with the type
+annotation:
+
+```rust,ignore
+let s: String = "static".try_into().unwrap().clone();
+//              ^^^^^^^^^^^^^^^^^^^ cannot infer type for `T`
+// note: type must be known at this point
+```
+
+while the equivalent code with `.try_conv::<_>` does compile:
+
+```rust
+# use wyz::conv::TryConv;
+let s = "static".try_conv::<String>().unwrap().clone();
+```
+
+## Conversion as traits
+
+Bounding a type with `TryConv` will not compile, because the trait itself gives
+no information:
+
+```rust,ignore
+# use wyz::conv::TryConv;
+fn lift<T: TryConv>(src: T) -> String {
+  src.try_conv::<String>().clone()
+//    ^^^^^^^^ the trait `From<T>` is not implemented for `String`
+// help: consider adding a `where String: From<T>` bound
+// note: required because of the requirements on the impl of `Into<String>` for `T`
+// note: required because of the requirements on the impl of `TryFrom<T>` for `String`
+}
+```
+
+This can be fixed by adding the clause `where String: TryFrom<T>`, or by using
+the bound `TryInto`:
+
+```rust
+# use std::convert::TryInto;
+# use wyz::conv::TryConv;
+fn lift<T: TryInto<String>>(src: T) -> String {
+  src.try_conv::<String>().ok().unwrap().clone()
+}
+```
+
+The `TryInto<T>` trait bound makes available both the `TryInto::<T>::try_into`
+method and the `TryConv::try_conv::<T>` method.
+
+[`TryFrom`]: https://doc.rust-lang.org/std/convert/trait.TryFrom.html
+[`TryInto`]: https://doc.rust-lang.org/std/convert/trait.TryInto.html
+**/
+pub trait TryConv: Sized {
+	/// Attempts to convert `self` into a target type.
+	///
+	/// This method runs `<Self as TryInto<T>>::try_into` on `self` to produce
+	/// the desired output. The only difference between using
+	/// `TryConv::try_conv` and `TryInto::try_into` is where the target type is
+	/// placed in the name; `.try_conv()` can be used in intermediate positions
+	/// of an expression, while `.try_into()` cannot.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use wyz::conv::TryConv;
+	///
+	/// let t = "hello".try_conv::<String>().unwrap();
+	/// ```
+	fn try_conv<T: Sized>(self) -> Result<T, Self::Error> where Self: TryInto<T> {
+		<Self as TryInto<T>>::try_into(self)
+	}
+}
+
+impl<T: Sized> TryConv for T {}
